9843dc60fc114272e489eb7393be8cfe2f308ff4
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2022-06-20 22:55:00 -0400

    ChakraUI and React Hook Form integration complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: `Keaton Type 1`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-datocms',
-    options: {
-      "apiToken": "11c7cea5158e2d069fa32beabb9c87"
-    }
-  }]
-};
+	siteMetadata: {
+		title: `Keaton Type 1`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-source-datocms',
+			options: {
+				apiToken: '11c7cea5158e2d069fa32beabb9c87',
+			},
+		},
+		'@chakra-ui/gatsby-plugin',
+	],
+}
